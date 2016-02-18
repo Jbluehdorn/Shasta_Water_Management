18b461d155812850e197b15a8de226c4a393e191
Bluehdoj <bluehdoj@otc.edu>
2016-02-18 14:41:53 -0600

    Menu System is now working

--- Shasta Water Management/Shasta Water Management/Scripts/sw-app.js
+++ Shasta Water Management/Shasta Water Management/Scripts/sw-app.js
@@ -1,4 +1,12 @@
 ﻿angular.module('shasta-water', [])
-    .directive('test', function() {
-        
+    //Holds the navigation information for the page
+    .directive('swNavigation', function () {
+        return {
+            restrict: 'A',
+            controller: ['$scope', function ($scope) {
+                $scope.categories = ['Home', 'Search', 'Add'];
+
+                $scope.selected = $scope.categories[0];
+            }]
+        }
     });
