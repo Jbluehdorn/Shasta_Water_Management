21dfb662795f7439a81374776f9c3a015613b88e
desquid <bluehdoj@otc.edu>
2016-03-17 14:14:00 -0500

    Changes

---
+++ Shasta Water Management/Shasta Water Management/Scripts/sw-profile.js
@@ -0,0 +1,87 @@
+﻿angular.module('customer-profile', [])
+    .controller('profileCtrl', ['$scope', function ($scope) {
+        $scope.customer = customer;
+        $scope.customer.LastServiceDate = new Date(parseInt($scope.customer.LastServiceDate.substr(6)));
+
+        //Prevents issues with setting the date directly
+        $scope.customer.NextServiceDate = new Date($scope.customer.LastServiceDate);
+        //Adds the service interval dates to the last service date
+        $scope.customer.NextServiceDate = new Date($scope.customer.NextServiceDate.setDate($scope.customer.NextServiceDate.getDate()  +  $scope.customer.ServiceInterval));
+
+        //Checks if the service date is before or after today
+        $scope.withinServiceDate = function() {
+            var today = new Date();
+
+            if ($scope.customer.NextServiceDate > today) {
+                return true;
+            }
+            return false;
+        }
+
+        $scope.remove = function (equipment) {
+            swal({
+                title: "Are you sure?",
+                text: "This equipment will be deleted",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Delete",
+                closeOnConfirm: false
+            },
+            function (confirmed) {
+                if (confirmed) {
+                    $scope.customer.Equipment.splice($scope.customer.Equipment.indexOf(equipment), 1);
+                    $scope.$apply();
+                    swal("Deleted!", "The equipment was been successfully removed", "success");
+                }
+            });
+        }
+    }])
+    //makes an editable input type=text
+    .directive('editableField', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '/Template/EditableTemplate.html',
+            scope: {
+                field: '=',
+                header: '='
+            },
+            controller: ['$scope', function ($scope) {
+                $scope.editable = false;
+
+                $scope.changeEditMode = function () {
+                    if (!$scope.editable) {
+                        $scope.fieldCopy = angular.copy($scope.field);
+                    } else {
+                        $scope.field = $scope.fieldCopy;
+                    }
+
+                    $scope.editable = !$scope.editable;
+                }
+            }]
+        }
+    })
+    //makes an editable textarea
+    .directive('editableTextarea', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '/Template/EditableTextareaTemplate.html',
+            scope: {
+                field: '=',
+                header: '='
+            },
+            controller: ['$scope', function ($scope) {
+                $scope.editable = false;
+
+                $scope.changeEditMode = function () {
+                    if (!$scope.editable) {
+                        $scope.fieldCopy = angular.copy($scope.field);
+                    } else {
+                        $scope.field = $scope.fieldCopy;
+                    }
+
+                    $scope.editable = !$scope.editable;
+                }
+            }]
+        }
+    });
