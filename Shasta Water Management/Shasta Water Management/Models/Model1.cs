f6db98a0c79a7fd11ad61c1c6622e12767d18189
HUNTER LADOUCEUR <hl0606674@otc.edu>
2016-03-21 17:06:22 -0500

    New models based on Database

---
+++ Shasta Water Management/Shasta Water Management/Models/Model1.cs
@@ -0,0 +1,79 @@
+namespace Shasta_Water_Management
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<CustEquip> CustEquips { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Equipment> Equipments { get; set; }
+        public virtual DbSet<Service> Services { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CustEquip>()
+                .Property(e => e.SerialNum)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.CellPhoneNum)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.HomePhoneNum)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.Address)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.City)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.Zip)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.Notes)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Equipment>()
+                .Property(e => e.Type)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Equipment>()
+                .Property(e => e.ModelNum)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Equipment>()
+                .Property(e => e.Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Service>()
+                .Property(e => e.StartLoc)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Service>()
+                .Property(e => e.EndLoc)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Service>()
+                .Property(e => e.Diagnostics)
+                .IsUnicode(false);
+        }
+    }
+}
